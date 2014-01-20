80e8945701fb423e1ff6dc424a430d3fa2c52fde
Andre Giron <agiron123@gmail.com>
2014-01-20 16:19:47 -0500

    Added documentation for parameters for circle.py. Follow this notation in the future.

--- Circle.py
+++ Circle.py
@@ -4,7 +4,10 @@ import numpy as np
 class Circle(Wall):
     """Subclass of wall. Represents a circle"""
     def __init__(self,center,radius):
-        """Create a circle with the given center and radius"""
+        """Create a circle with the given center and radius
+        :param center: The center of the circle.
+        :param radius: The radius of the circle.
+        """
         self.center=center
         self.radius=radius
 
