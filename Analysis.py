b7948a2d3a307be224ca1635c04a3545451f441e
Viral Patel <viral9793@gmail.com>
2014-01-31 00:33:55 -0500

    Updated to Statistics and Analysis. Statistics updates its dictionary and stat list. Analysis has partial implementation of output generation

--- Analysis.py
+++ Analysis.py
@@ -1,3 +1,7 @@
+import csv
+##import os this can be used later if csv files with the same name already exist
+import matplotlib.pyplot as plt
+
 class Analysis(object):
     """Used to output information from the simulation. Has access to all of the information held in
         Statistics. This information can be outputted in raw form, or analyzed further, including transformation
@@ -9,3 +13,19 @@ class Analysis(object):
         Analysis.generate_graphs = generate_graphs
         Analysis.generate_output = generate_output
 
+    def generate_output(self, statistics):
+        data = statistics.data
+        filename = csv.open('Simulation_Data.csv','wb')
+        writer = csv.writer(filename)
+        writer.writerow(data.keys())
+        writer.writerow(data.values())
+
+        writer.writerow("Stats")
+        stat_list = statistics.stat_list
+        #TODO: dump the data from a statlist
+
+        filename.close()
+
+
+    def generate_graphs(self, statistics):
+    #TODO: check the types of desired graphs and how to graph them
--- Statistics.py
+++ Statistics.py
@@ -8,3 +8,46 @@ class Statistics(object):
         """Initialize the statistics object with the given dictionary and update function."""
         self.info = info
         Statistics.update = update
+        self.data = {'total_absorbed': 0, 'total_trapped': 0, 'avg_number_reflections': 0,
+                     'avg_number_interactions': 0, 'avg_azimuth': 0, 'avg_zenith': 0,
+                     'avg_wavelength': 0, 'number_photons': 0, 'total_number_reflections': 0,
+                     'total_number_interactions': 0}
+        self.total_wavelength = 0
+        self.total_azimuth = 0
+        self.total_zenith = 0
+        self.stat_list = []
+
+    def update(self, stat):
+        if stat is not None:
+
+            self.stat_list.append(self, stat)
+
+            self.data['number_photons'] += 1
+
+            if stat.absorbed:
+                self.data['total_absorbed'] += 1
+
+            if stat.trapped:
+                self.data['total_trapped'] += 1
+
+            self.data['total_number_reflections'] += stat.reflections
+
+            self.data['total_number_interactions'] += stat.interactions
+
+            self.data['avg_number_reflections'] = self.data['total_number_reflections']/self.data['number_photons']
+
+            self.data['avg_number_interactions'] = self.data['total_number_interactions']/self.data['number_photons']
+
+            self.total_wavelength += stat.wavelength
+            self.data['avg_wavelength'] = self.total_wavelength / self.data['number_photons']
+
+            self.total_azimuth += stat.azimuth
+            self.data['avg_azimuth'] = self.total_azimuth / self.data['number_photons']
+
+            self.total_zenith += stat.zenith
+            self.data['avg_zenith'] = self.total_zenith / self.data['number_photons']
+
+
+
+
+
