56dd9cd0ba4631b88cff2144caf24e3306f734a8
Viral Patel <viral9793@gmail.com>
2014-02-03 16:27:34 -0500

    Updated Analysis and Stat classes. Will print Stat data to CSV files

--- Analysis.py
+++ Analysis.py
@@ -13,6 +13,7 @@ class Analysis(object):
         Analysis.generate_graphs = generate_graphs
         Analysis.generate_output = generate_output
 
+<<<<<<< Updated upstream
     def generate_output(self, statistics):
         data = statistics.data
         filename = csv.open('Simulation_Data.csv','wb')
@@ -25,7 +26,42 @@ class Analysis(object):
         #TODO: dump the data from a statlist
 
         filename.close()
+=======
+    #This method creates a CSV file with all of the data from a simulation
+    def generate_output(self, statistic):
+        #Copies the data dictionary from a statistic
+        data = statistic.data
+
+        #Creates a CSV file to write to or overwrites an existing file with the same name
+        file_name = open('Simulation_Data.csv', 'wb')
+
+        #Creates the writer object for a given file
+        writer = csv.writer(file_name)
+
+        #Write the string to the first row of the CSV file
+        writer.writerow(["Compiled Data"])
+
+        #Writes the keys of the data dictionary to the second row of the CSV file
+        writer.writerow(data.keys())
+        #Writes the values of the data dictionary to the third row of the CSV file
+        writer.writerow(data.values())
+
+        #Writes the word "Stats" to the fourth row of the CSV file
+        writer.writerow(["Stats"])
+
+        #Copies the stat_list from statistics
+        stat_list = statistic.stat_list
+
+        #This will up date each stat's dictionary and then print its contents in the CSV file
+        for stat in stat_list:
+            stat_dictionary = stat.update_dictionary()
+            writer.writerow(stat_dictionary.keys())
+            writer.writerow(stat_dictionary.values())
+
+        #Closes the CSV file
+        file_name.close()
+>>>>>>> Stashed changes
 
 
     def generate_graphs(self, statistics):
-    #TODO: check the types of desired graphs and how to graph them
+        #TODO: check the types of desired graphs and how to graph them
