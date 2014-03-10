63caa3768ca8638fb6b453637ed9ed92b6b5cdbd
Andre Giron <agiron123@gmail.com>
2014-03-10 16:04:08 -0400

    Committing my code so that we can make some modules. Added attributes for XML datatype

--- Main.py
+++ Main.py
@@ -16,9 +16,17 @@ def Main():
 
     print(arguments)
 
+""" Uncomment when done debugging this part
+    print "FINDING OUT WHAT'S IN ARG[I] \n"
+
     for i in range(0, len(arguments)):
+        print arguments[i]
         output_settings = OutputSettings(arguments[i]["OutputSettings"])
         graph_settings = GraphSettings(arguments[i]["OutputSettings"]["GraphSettings"])
+
+    print "NO LONGER FINDING OUT WHAT'S IN ARGUMENTS[I]\n"
+"""
+
     #statistics = Statistics()
     #Simulation.run(arguments, statistics)
 
