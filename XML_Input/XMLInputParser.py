c535458d279ffc64faa2672291b760088eadf8eb
Andre Giron <agiron123@gmail.com>
2014-03-25 15:51:42 -0400

    Pushing XML input parser. See TODO at the bottom for what needs to be done.

--- XML_Input/XMLInputParser.py
+++ XML_Input/XMLInputParser.py
@@ -18,10 +18,7 @@ class XMLInputParser(object):
 
         tokens = {}
         with open(file, 'rt') as f:
-            #xmlfile = unicode(f,'utf-8')
             tree = ET.parse(f)
-            #content = unicode(f.strip(codecs.BOM_UTF8), 'utf-8')
-            #tree = ET.parse(StringIO.StringIO(content))
 
         print("Testing out the recursive parse function: ")
         simulations = list()
@@ -55,6 +52,7 @@ class XMLInputParser(object):
             return None #return none because the parsing was not completely successful
         return simulations
 
+    #recursively parse the xml tree.
     def parseRecur(self, root):
         resultDict = {}
         global dataTypeDict
@@ -70,8 +68,9 @@ class XMLInputParser(object):
             resultDict[child_of_root.tag] = self.parseRecur(child_of_root)
         return resultDict
 
+    #compare the parsed xml file to a valid xml file
     def compareDict(self, validDict, parsed):
-        global dataTypeDict
+        global dataTypeDict #using a global dictionary to hold the datatypes that we need. Probably not one of my best ideas.
         missingkeylist = set()    #using set. As long as all the tags have different names, we're golden.
         for k,v in validDict.iteritems():
             if isinstance(v,dict):
@@ -82,7 +81,7 @@ class XMLInputParser(object):
             elif not parsed.has_key(k):
                 if k not in missingkeylist:
                     missingkeylist.add(k)
-            elif parsed.has_key(k):
+            elif parsed.has_key(k):    #do the type validation right here.
                 print str(parsed[k].encode('utf-8')) #IMPORTANT LINE HERE. Make sure we encode to unicode, or we won't be able to print it out.
                 #Need to do this for when you are printint out or writing to disk. see farmdev.com/talks/unicode for more information.
                 #print "Parsed[k]: " + str(parsed[k]) + " dataTypeDict[k]: " + str(dataTypeDict)
@@ -90,6 +89,7 @@ class XMLInputParser(object):
                     #print ("Datatype Mismatch")
         return missingkeylist
 
+    #Essentially just has the base cases for the compareDict method.
     def compareDictHelper(self, validDict, parsed, missingkeylist):
         if isinstance(validDict, dict) and not isinstance(validDict, dict):
             if validDict not in missingkeylist:
@@ -103,3 +103,26 @@ class XMLInputParser(object):
         elif isinstance(parsed, dict) and isinstance(validDict, dict):
             missingkeylist.update(self.compareDict(validDict, parsed))
         return missingkeylist
+
+    '''TODO:
+    -Make sure that the datatypes are the proper datatypes for python to be using
+
+    -Make sure that each xml element is validated against the parsed datatypes
+    -Going to need to check that the input file has the right datatype specified, and that it matches the acceptable datatypes for that element in the validator.
+
+
+    Some kind of switch, ifelse, etc to check the datatype (see below)
+
+    def checkParsedValue(parsedValue, parsedType)
+        if parsedType == "int"
+            return type(parsedValue) == int
+        elif parsedType == "string"
+            return type(parsedType)n == str
+        etc. for the rest of the datatypes that we need...
+
+    -Make sure that each xml element is within an acceptable range.
+        -This range is probably going to have to be specified in the xml_validator file
+
+    -Add documentation so that it works with Sphinx. Did not get around to putting in the docstrings needed for that.
+
+    '''
